7bc5ab8a2c45b1cc6b690a1e226ff0cf8df3ab9d
jc473262cc2511 <william.hunter@my.jcu.edu.au>
2025-05-31 18:06:50 +1000

    Add password check program

---
+++ prac_02/password_stars.py
@@ -0,0 +1,19 @@
+"""
+William Hunter
+CP1404
+"""
+
+def main():
+    """Print stars same length as password"""
+    get_valid_password()
+
+def get_valid_password():
+    password = input("Password: ")
+
+    while len(password) <= 5:
+        print("Password must be greater than 5 characters long")
+        password = input("Password: ")
+
+    print('*'*len(password))
+
+main()
