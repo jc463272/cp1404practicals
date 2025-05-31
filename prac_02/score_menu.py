313b8b463f4d423a1f975774f133bde14728b1b5
jc473262cc2511 <william.hunter@my.jcu.edu.au>
2025-06-01 09:04:38 +1000

    Added print result function and tested program

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,31 +1,59 @@
 """
 William Hunter
 """
-from prac_02.score import determine_grade
 
 MENU = "(G)et valid score,(P)rint Result, (S)how stars, (Q)uit"
 
+
 def main():
+    """User menu"""
+    score = 0
     print(MENU)
     choice = input(">>>").upper()
     while choice != "Q":
         if choice == "G":
-            score = int(input("Score: "))
-        if choice == "P":
-            determine_grade(score)
-        if choice == "S":
+            score = get_valid_score()
+        elif choice == "P":
+            print_result(score)
+        elif choice == "S":
             if score is not None:
                 print_stars(score)
-            else:
-                print("Invalid score")
         else:
-            print("Invalid choice")
-    print("Farewell!")
+            print("Invalid score")
+        print(MENU)
+        choice = input(">>>").upper()
+    print("Farewell")
 
-def determine_grade(score):
-    pass
+
+def get_valid_score():
+    """Prompt user for valid score"""
+    score = int(input("Score: "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = int(input("Score: "))
+    return score
+
+
+def print_result(score):
+    """print result of score"""
+    result = determine_result(score)
+    print(f"Your score is: {score}, your result: {result}")
+
+
+def determine_result(score):
+    """Determine result from score"""
+    if score < 0 or score > 100:
+        return "Invalid Score"
+    if score < 50:
+        return "Bad"
+    if score < 90:
+        return "Passable"
+    return "Excellent"
+
 
 def print_stars(score):
-    print("*"*score)
+    """print stars length of score"""
+    print("*" * score)
+
 
 main()
