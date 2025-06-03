95c6f6b8813e2cd7d40485e1cbe30e05034e3205
jc473262cc2511 <william.hunter@my.jcu.edu.au>
2025-06-03 18:07:28 +1000

    Answered questions and added code for random number generator

---
+++ prac_03/randoms.py
@@ -0,0 +1,43 @@
+
+"""William Hunter"""
+
+
+"""Line 1"""
+#What did you see on line 1?
+#17
+#What was the smallest number you could have seen, what was the largest?
+#Smallest was 5, largest was 29
+
+"""Line 2"""
+#What did you see on line 2?
+#5
+
+#What was the smallest number you could have seen, what was the largest?
+#Smallest was 5, largest was 9
+
+#Could line 2 have produced a 4?
+#No
+
+"""Line 3"""
+# What did you see on line 3?
+# 2.902707850137771
+# What was the smallest number you could have seen, what was the largest?
+#2.5
+
+"""Produce a random number between 1 - 100"""
+import random
+
+print(random.randint(1,100))
+
+
+
+
+
+
+
+
+
+
+
+
+
