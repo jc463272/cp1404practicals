4625677033cff336b4f7c19917c91c8f296f6693
jc473262cc2511 <william.hunter@my.jcu.edu.au>
2025-06-07 15:40:47 +1000

    Reduce length of comments

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,14 +3,16 @@ CP1404
 Answer all following questions
 """
 
-#1 Write code that asked the user for their name and then opens a file called name.txt and writes that name to it.
+#1 Write code that asked the user for their name and
+# then opens a file called name.txt and writes that name to it.
 
 out_file = open("name.txt", 'w')
 name = input("Enter your name: ")
 print(name, file=out_file)
 out_file.close()
 
-#2 In the same file but as if it were a separate program write a code that opens "name.txt"
+#2 In the same file but as if it were a separate program
+# write a code that opens "name.txt"
 # and reads the name and prints the output
 
 in_file = open("name.txt", 'r')
@@ -19,7 +21,9 @@ print(f"Hi {in_file.read().strip('\n')}!")
 in_file.close()
 
 #3 Create a text file called numbers.txt.
-# write code that opens numbers.txt, read only the first two numbers, adds them together then prints result
+# write code that opens numbers.txt,
+# read only the first two numbers,
+# adds them together then prints result
 
 with open("numbers.txt",'r') as in_file:
     number_1 = int(in_file.readline())
