d2f42e0c0b49d7d59bfede19e3904bc6baf529a7
jc473262cc2511 <william.hunter@my.jcu.edu.au>
2025-06-10 15:22:25 +1000

    included two functions to load subject data and then display subject data nicely.

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -1,29 +1,32 @@
 """
 CP1404/CP5632 Practical
 Data file -> lists program
+William Hunter
 """
 
 FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data()
-    print(data)
+    """Read subjects and display"""
+    subjects = load_subject()
+    display_subjects(subjects)
 
 
-def load_data():
-    """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(FILENAME)
-    for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
-    input_file.close()
+def load_subject():
+    """load data from file formatted like: subject,lecturer,number of students."""
+    subjects = []
+    with open(FILENAME,'r') as input_file:
+        for line in input_file:
+            line = line.strip()
+            parts = line.split(',')
+            parts[2] = int(parts[2])
+            subjects.append(parts)
+        return subjects
 
+def display_subjects(subjects):
+    """Display subjects"""
+    for subject in subjects:
+        print(f"{subject[0]} is taught by {subject[1]:12} and has {subject[2]:3} students.")
 
 main()
