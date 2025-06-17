9c40813ea4095fcccec19daa983bcd594c2684d6
jc473262cc2511 <william.hunter@my.jcu.edu.au>
2025-06-17 18:10:33 +1000

    Add LBYL loop and formatted print loop

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,18 +5,31 @@ File needs reformatting
 """
 
 state_code_to_name = {"QLD": "Queensland",
-                "NSW": "New South Wales",
-                "NT": "Northern Territory",
-                "WA": "Western Australia",
-                "ACT": "Australian Capital Territory",
-                "VIC": "Victoria",
-                "TAS": "Tasmania",
-                "SA": "South Australia"}
+                      "NSW": "New South Wales",
+                      "NT": "Northern Territory",
+                      "WA": "Western Australia",
+                      "ACT": "Australian Capital Territory",
+                      "VIC": "Victoria",
+                      "TAS": "Tasmania",
+                      "SA": "South Australia"}
 
-state = input("Enter short state: ").upper()
-while state != "":
-    if state in state_code_to_name:
-        print(state, "is", state_code_to_name[state])
-    else:
+length_of_code = max(len(code) for code in state_code_to_name.keys())
+
+for code, name in state_code_to_name.items():
+    print(f"{code:{length_of_code}} is \t{name}")
+
+is_valid_state = False
+while not is_valid_state:
+    try:
+        state = input("Enter state code: ").upper()
+        if state in state_code_to_name:
+            print(f"{state:{length_of_code}} is \t{state_code_to_name[state]}")
+        elif state not in state_code_to_name:
+            print("Invalid state code")
+        else:
+            is_valid_state = True
+    except ValueError:
         print("Invalid short state")
-    state = input("Enter short state: ").upper()
+    except TypeError:
+        print("Invalid short state")
+
