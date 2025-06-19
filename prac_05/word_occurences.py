eb845e2cf09c997e90f1a73c6d9e48cac3206e6a
jc473262cc2511 <william.hunter@my.jcu.edu.au>
2025-06-19 12:38:29 +1000

    add if statement to prevent no input

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -9,12 +9,13 @@ word_to_count = {}
 sentence = input("Enter sentence: ").lower()
 words = sentence.split()
 
-for word in words:
-    word_to_count[word] = word_to_count.get(word, 0) + 1
+if sentence != "":
+    for word in words:
+        word_to_count[word] = word_to_count.get(word, 0) + 1
 
-words = list(word_to_count.keys())
-words.sort()
+    words = list(word_to_count.keys())
+    words.sort()
 
-max_length = max((len(word) for word in words))
-for word in words:
-    print(f"{word:{max_length}} : {word_to_count[word]}")
+    max_length = max((len(word) for word in words))
+    for word in words:
+        print(f"{word:{max_length}} : {word_to_count[word]}")
