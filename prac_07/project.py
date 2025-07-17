24b2b1a3aecb9ffda56eeb1c48cef892915dcb27
jc473262cc2511 <william.hunter@my.jcu.edu.au>
2025-07-17 17:44:35 +1000

    Add datetime module init and str

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,6 +1,6 @@
 """
 Estimate: 50 mins
-Actual:
+Actual: 30 mins
 """
 
 import datetime
@@ -12,15 +12,15 @@ class Project:
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
         """Initialize a project instances."""
         self.name = name
-        self.start_date = start_date
+        self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
         self.priority = int(priority)
         self.cost_estimate = float(cost_estimate)
         self.completion_percentage = int(completion_percentage)
 
     def __repr__(self):
         """String representation of a project."""
-        return (f"{self.name}, start: {self.start_date}, priority {self.priority}, "
-                f"estimate: {self.cost_estimate}, completion: {self.completion_percentage}%")
+        return (f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, priority {self.priority}, "
+                f"estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%")
 
     def __lt__(self, other):
         """Sort projects by priority."""
