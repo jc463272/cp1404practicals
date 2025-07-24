d1a2ebf36b1fa91d296f71fac20e793ac0966764
jc473262cc2511 <william.hunter@my.jcu.edu.au>
2025-07-24 16:07:02 +1000

    Complete miles conversion program

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,45 @@
+"""
+CP1404 - William Hunter
+Estimate: 30 mins
+Actual: 60 mins
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+FACTOR_MILES_TO_KM = 1.60934
+
+
+class MilesConverterApp(App):
+    """Kivy App for converting miles to kilometres."""
+    output_km = StringProperty()
+
+    def build(self):
+        """Build the Kivy app from the kv file."""
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
+
+    def handle_calculate(self, text):
+        """Handle calculation when button pressed or input entered."""
+        miles = self.convert_to_number(text)
+        self.update_result(miles)
+
+    def handle_increment(self, text, change):
+        """Handle up/down button press, updated the text input value."""
+        miles = self.convert_to_number(text) + change
+        self.root.ids.input_miles.text = str(miles)
+
+    def update_result(self, miles):
+        self.output_km = str(miles * FACTOR_MILES_TO_KM)
+
+    @staticmethod
+    def convert_to_number(text):
+        """Convert text to float or default parameter 0.0"""
+        try:
+            return float(text)
+        except ValueError:
+            return 0.0
+
+MilesConverterApp().run()
