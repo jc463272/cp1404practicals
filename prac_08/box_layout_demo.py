a10c53089719cd274cdf295ef736880ed9c69500
jc473262cc2511 <william.hunter@my.jcu.edu.au>
2025-07-24 12:20:34 +1000

    Complete kivy box layout walktrhough

---
+++ prac_08/box_layout_demo.py
@@ -0,0 +1,24 @@
+"""
+CP1404
+Modified by William Hunter
+Estimated: 15 mins
+Actual: 20 mins
+"""
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class BoxLayoutDemo(App):
+    def build(self):
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+    def handle_greet(self):
+        self.root.ids.output_label.text = f"Hello, {self.root.ids.input_name.text}"
+
+    def clear_greet(self):
+        self.root.ids.output_label.text = ""
+
+
+BoxLayoutDemo().run()
