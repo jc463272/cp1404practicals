3c0b4e33af79a1a9d21c1d56dacca53f9dfafc90
jc473262cc2511 <william.hunter@my.jcu.edu.au>
2025-07-24 16:33:02 +1000

    Complete dynamic labels program

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,36 @@
+"""
+CP1404 - William Hunter
+Estimate:
+Actual:
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+from kivy.properties import StringProperty
+
+FILENAME = 'dynamic_labels.kv'
+
+class NameLabels(App):
+    """Main program Kivy app - dynamically create label for names in list."""
+    name = StringProperty()
+    def __init__(self, **kwargs):
+        """Initialize app and define list of names."""
+        super().__init__(**kwargs)
+        self.names = ['Kyle','David','Jeff','William']
+
+    def build(self):
+        """Build display and populate dynamic labales."""
+        self.title = 'Name Labelling'
+        self.root = Builder.load_file(FILENAME)
+        self.create_widgets()
+        return self.root
+
+    def create_widgets(self):
+        """Create dynamic labels for names in defined list."""
+        for name in self.names:
+            # create label for each data entry
+            name_label = Label(text=name)
+            self.root.ids.name_entries.add_widget(name_label)
+
+NameLabels().run()
